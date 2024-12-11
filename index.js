da19a73ea3ddef760c9dc0eccbb6719e79ea29b6
Sydney Tsin <sage.tsin@gmail.com>
2024-12-11 01:46:15 -0500

    Move vinyls off screen again

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 $(document).ready(function(){
-    var half_width = $(".rotating-img-left ").width() / 2;
-    $(".rotating-img-left ").css("left", -half_width);
-});
-$(document).ready(function(){
+    var half_width = $(".rotating-img-left").width() / 2;
+    $(".rotating-img-left").css("left", -half_width);
     var half_width = $(".rotating-img-right ").width() / 2;
-    $(".rotating-img-right ").css("right", -half_width);
+        $(".rotating-img-right").css("right", -half_width);
 });
