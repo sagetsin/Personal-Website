2225241047a5541efd63f1ba351504600d5ee3a0
Sydney Tsin <sage.tsin@gmail.com>
2024-12-10 23:24:09 -0500

    Spotify play with button click

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ const playButton = document.getElementById('playButton');
 const spotifyPlayer = document.getElementById('player');
 
 playButton.addEventListener('click', () => {
-    spotifyPlayer.style.display = 'block'; // Show the player
-    spotifyPlayer.src += '?autoplay=1'; // Append autoplay parameter
-    spotifyPlayer.style.display = 'none'; // Hide the player
+    spotifyPlayer.contentWindow.postMessage({method: 'play'}, '*');
+//    spotifyPlayer.style.display = 'block'; // Show the player
+//    spotifyPlayer.src += '?autoplay=1'; // Append autoplay parameter
+//    spotifyPlayer.style.display = 'none'; // Hide the player
+    console.log('Music is playing...');
 });
