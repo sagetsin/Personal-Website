ab36096eb0c48d1d551134294c3016b5ab07f572
Sydney Tsin <sage.tsin@gmail.com>
2024-12-12 14:59:16 -0500

    Vinyls and spotify fixing

--- index.js
+++ index.js
@@ -3,18 +3,5 @@
 
         // Open the pop-up
         function openPopup() {
-            popup.style.display = "block";
             window.open("https://sagetsin.github.io/main/pages/spotify-player.html", "Spotify Player", "width=500,height=300");
-        }
-
-        // Close the pop-up
-        function close() {
-            popup.style.display = "none";
-        }
-
-        // Optionally close the popup if the overlay is clicked
-        function offPop(event) {
-            if (event.target === popup) {
-                popup.style.display = "none";
-            }
         }
