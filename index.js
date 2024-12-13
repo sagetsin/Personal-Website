9f4cb5d5fb3b2f41d1f9113c7be65b35ce29d17e
Sydney Tsin <sage.tsin@gmail.com>
2024-12-13 15:36:26 -0500

    Repair broken JS for opening player

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
  // Get elements
         const popup = document.getElementById("popup");
-        var open = false;
+        var popupOpen = false;
 
         // Open the pop-up
         function openPopup() {
-            if(!open) {
+            if(!popupOpen) {
                 window.open("https://sagetsin.github.io/main/pages/spotify-player.html", "Spotify Player", "width=500,height=300");
+                popupOpen = true;
             }
         }
