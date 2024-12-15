994f0602f73f215dc33ad655049cb946fa0fe00e
Sydney Tsin <sage.tsin@gmail.com>
2024-12-14 23:35:20 -0500

    Attempting vinyls again

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
  // Get elements
-        const left = document.getElementById("vinyl-left");
-        const right = document.getElementById("vinyl-right");
+        var left = document.getElementById("vinyl-left");
+        var right = document.getElementById("vinyl-right");
         var popupOpen = false;
         let popupWindow = null;
 
@@ -9,22 +9,18 @@
             if(!popupOpen || popupWindow.closed) {
                 popupWindow = window.open("https://sagetsin.github.io/main/pages/spotify-player.html", "Spotify Player", "width=500,height=300");
                 popupOpen = true;
-                left.class = "rotate-left";
-                right.class = "rotate-right";
+                left.classList.add("rotate-left");
+                right.classList.add("rotate-right");
             }
         }
 
         function closePopup(){
-        console.log("attempting to close");
-            if(popupOpen && !popupWindow.closed){
             console.log("time to close");
-                popupWindow.close();
-                popupWindow = null;
-                popupOpen = false;
-                left.class = "vinyl-left";
-                right.class = "vinyl-right";
-
-            }
+            popupWindow.close();
+            popupWindow = null;
+            popupOpen = false;
+            left.classList.add("vinyl-left");
+            right.classList.add("vinyl-right");
         }
 
         //Sparkles
